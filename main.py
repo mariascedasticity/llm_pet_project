82cdb38bcba116ef7853f2878263f8a3b1911c04
mariascedasticity <gmaria1904@gmail.com>
2023-08-15 18:18:47 +0200

    Added openai

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import openai
+import os
+
+# Get the API key from the environment variable
+openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+# Check if the API key is present
+if not openai_api_key:
+    raise ValueError("OPENAI_API_KEY is not set in the environment variables")
+
+openai.api_key = openai_api_key
 
 app = FastAPI()
+
+class TextRequest(BaseModel):
+    prompt: str
 
 @app.get("/")
 def read_root():
@@ -9,3 +24,12 @@ def read_root():
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
+@app.post("/complete-text/")
+def complete_text(request: TextRequest):
+    prompt = request.prompt
+    try:
+        response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=150)
+        return {"completed_text": response.choices[0].text.strip()}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/Users/gmaria/willowhealth/willow_health/llm_project/venv"
+setenv VIRTUAL_ENV "/Users/gmaria/willowhealth/willow_health/llm_project/llm_pet_project/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
